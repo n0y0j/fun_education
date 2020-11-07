2813c5afd906372baeaa9f0f0049748f81ddf98a
n0y0j <n0y0j77@gmail.com>
2020-11-07 11:58:29 +0900

    feat: Implement Recycler Adapter.

---
+++ 1_delivery_app/educational_structure/app/src/main/java/com/example/delivery_app/DetailFragment/Adapter/KoreaRecyclerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.delivery_app.DetailFragment.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.delivery_app.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class KoreaRecyclerAdapter extends RecyclerView.Adapter<KoreaViewHolder> {
+
+    private HashMap<String, ArrayList<String>> product;
+
+    private int[] src = {
+            R.drawable.kimbap, R.drawable.kimchi, R.drawable.rice
+    };
+
+    @NonNull
+    @Override
+    public KoreaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.detail_item_card, parent, false);
+        KoreaViewHolder viewHolder = new KoreaViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull KoreaViewHolder holder, int position) {
+        holder.image.setImageResource(src[position]);
+        holder.name.setText(product.get("name").get(position));
+        holder.score.setText(product.get("score").get(position));
+        holder.min_price.setText(product.get("min_price").get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return product.get("name").size();
+    }
+}
