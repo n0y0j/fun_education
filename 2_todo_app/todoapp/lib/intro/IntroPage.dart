4b220bd0f03ba7d4e52277fe9f1be16d7f99b8ad
n0y0j <n0y0j77@gmail.com>
2020-12-27 20:48:35 +0900

    feat: Implement intro page.

---
+++ 2_todo_app/todoapp/lib/intro/IntroPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Image.asset(
+            'assets/images/logo.png',
+            height: 250,
+          ),
+        ),
+      ),
+    );
+  }
+}
